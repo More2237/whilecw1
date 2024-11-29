ea6268e076929d21958a290056c9fc7a9f88a2fc
More2237 <noorallyrahmonov2308@gmail.com>
2024-11-29 17:56:32 +0500

    mnb

---
+++ whilecw/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace while2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 33;
+            while (x <= y)
+            {
+             if (x % 2 == 0)
+             {
+                Console.WriteLine(x);
+             }
+
+             x++;
+            }
+            {
+                
+            }
+            
+        }
+    }
+}
